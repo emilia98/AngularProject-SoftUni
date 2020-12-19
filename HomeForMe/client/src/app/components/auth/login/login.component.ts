5f230de09d04137b7b207ee0964f96e05566aa06
emilia98 <emilia_nedialkova@abv.bg>
2020-12-19 09:38:59 +0200

    add validations + notifications to login

--- HomeForMe/client/src/app/components/auth/login/login.component.ts
+++ HomeForMe/client/src/app/components/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -9,19 +11,36 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
   model: any = {};
+  errors: any = [];
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService, 
+    private toastrService: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
-  login() {
+  login(loginForm: NgForm) {
+    this.errors = [];
+
     this.authService.login(this.model)
-      .subscribe(response => {
-        console.log(response);
+      .subscribe((response : any) => {
+        if (response.hasSuccess) {
+          this.toastrService.success(response.message, 'Authentication successful!')
+        }
         this.router.navigateByUrl('/');
-      }, error => {
-        console.log(error)
+        loginForm.reset()
+      }, (error : any) => {
+        if (error.error.hasError) {
+          this.toastrService.error(error.error.message)
+        } else if (error.error.errors) {
+          this.errors = error.error.errors;
+          this.toastrService.error("Fill up the form properly to log in!")
+        } else {
+          this.toastrService.error(error)
+        }
       })
   }
 }
